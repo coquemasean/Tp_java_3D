14971a2440ddbe032a1e7d87f780854d81478595
COQUEMA Sean <coquemasean@gmail.com>
2022-01-23 23:20:30 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Aeroport CharlesDeGaulle = new Aeroport ("CDG", "Charles de Gaulle", "France", 49.1, 2.1);
         World w = new World ("D:/ENSEA/2021-2022/Tp_java_3D/data/airport-codes_no_comma.csv");
 
-        System.out.println(CharlesDeGaulle);
+        System.out.println("");
+        System.out.println("Found "+w.getList().size()+" airports."); // donne les larges aeroport
+
+        Aeroport paris = w.findNearestAirport(2.316,48.866); //un localistion à Paris
+        Aeroport cdg = w.findByCode("CDG");
+
+        double distance = w.distance(2.316,48.866,paris.getLongitude(),paris.getLatitude());
+        System.out.println(paris);
+        System.out.println(distance);
+
+        double distanceCDG = w.distance(2.316,48.866,cdg.getLongitude(),cdg.getLatitude());
+        System.out.println(cdg);
+        System.out.println(distanceCDG);
+
     }
 }
--- src/World.java
+++ src/World.java
@@ -9,49 +9,50 @@ public class World {
     public ArrayList<Aeroport> getList() {
         return list;
     }
-        public World(String filename) {
-            list = new ArrayList<Aeroport>();
-            try {
-                BufferedReader buf = new BufferedReader(new FileReader(filename));
-                String s = buf.readLine();
-                while (s != null) {
-                    s = s.replaceAll("\"", "");
-                    //Enleve les guillemets qui s ́eparent les champs de données GPS.
-                    String fields[] = s.split(",");
-                    // Une bonne id ́ee : placer un point d'arr^et ici pour du debuggage.
-                    if (fields[1].equals("large_airport")) {
-                        Aeroport large_a;
-                        list.add(large_a = new Aeroport(fields[9],fields[2],fields[5],Double.parseDouble(fields[12]),
-                                Double.parseDouble(fields[11])));
-                    }
-                    s = buf.readLine();
-                }
-            } catch (Exception e) {
-                System.out.println("Maybe the file isn't there ?");
-                System.out.println(list.get(list.size() - 1));
-                e.printStackTrace();
-            }
-        }
 
-    public double distance(double lo1, double la1, double lo2, double la2){
+    public World(String filename) {
+        list = new ArrayList<Aeroport>();
+        try {
+            BufferedReader buf = new BufferedReader(new FileReader(filename));
+            String s = buf.readLine();
+            while (s != null) {
+                s = s.replaceAll("\"", "");
+                //Enleve les guillemets qui s ́eparent les champs de données GPS.
+                String fields[] = s.split(",");
+                // Une bonne id ́ee : placer un point d'arr^et ici pour du debuggage.
+                if (fields[1].equals("large_airport")) {
+                    Aeroport large_a;
+                    list.add(large_a = new Aeroport(fields[9], fields[2], fields[5], Double.parseDouble(fields[12]),
+                            Double.parseDouble(fields[11])));
+                }
+                s = buf.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("Maybe the file isn't there ?");
+            System.out.println(list.get(list.size() - 1));
+            e.printStackTrace();
+        }
+    }
+
+    public double distance(double lo1, double la1, double lo2, double la2) {
         double norme = pow(la2 - la1, 2) + pow((lo2 - lo1) * cos(toRadians((la2 + la1) / 2)), 2);
         return norme;
     }
 
-    public Aeroport findNearestAirport(double lon_pos, double lat_pos){
+    public Aeroport findNearestAirport(double lon_pos, double lat_pos) {
         //Calcul la distance entre notre position le premier aeroport de la liste
-        double dist = distance(lon_pos,lat_pos,list.get(0).getLongitude(),list.get(0).getLatitude());
+        double dist = distance(lon_pos, lat_pos, list.get(0).getLongitude(), list.get(0).getLatitude());
         //Aeroport tampon
         Aeroport b = null;
         double index = 0;
 
-        for(Aeroport a : list){
+        for (Aeroport a : list) {
             //Prend la latitude et longitude ne l'aeroport de la liste
             double lat1 = a.getLatitude();
             double lon1 = a.getLongitude();
             //Calcul la distance entre notre position et un aeroport de la liste
-            double d = distance(lon_pos,lat_pos,lon1, lat1);
-            if(d < dist){
+            double d = distance(lon_pos, lat_pos, lon1, lat1);
+            if (d < dist) {
                 b = a;
                 dist = d;
             }
@@ -59,5 +60,15 @@ public class World {
         return b;
     }
 
+    //Recupere le code IATA
+    public Aeroport findByCode(String code) {
+        for (Aeroport aero : list) {
+            if (aero.getIATA().equals(code)) {
+                return aero;
+            }
+        }
+        System.out.println("Le code IATA ne correspond à aucun aeroport");
+        return null;    }
 }
+
 
